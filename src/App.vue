1048a81c5dc3505e5931b93660ff79560335c7c6
feefeefee <feiqikai@qq.com>
2023-08-30 15:40:51 +0800

    test: 测试提交

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref('大西瓜')
+</script>
+
+<template>
+  <div class="name">
+    {{ name }}哈哈哈哈
+  </div>
+</template>
+
+<style scoped>
+.name {
+  color:red
+}
+</style>
